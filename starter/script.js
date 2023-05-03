f1361f29c691f17a718c75ab6ad9e3abe1d100e8
isabele22 <108808972+isabele22@users.noreply.github.com>
2023-05-03 10:21:55 -0300

    Add files via upload

---
+++ starter/script.js
@@ -0,0 +1,50 @@
+const reviews = [
+    {
+        id: 1,
+        name:"Susan Smith",
+        job:"Web Develope",
+        image:"https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg",
+        text:"I'm baby meggings twrr healt goth +1 , Bicycle  rights tumeric chartreuse before they sold out chambray pop-up"
+    },
+
+    {
+        id:2,
+        name:"Anna Johson",
+        job:"Web Designer",
+        image: "https://images2.imgbox.com/2e/6e/JAMvTZ56_o.jpeg",
+        text: "Helvetica artisan kindfolk thundercats. Disrupt glossier gastropub deep v vice frazen.",
+    },
+
+    {
+        id:3,
+        name:"Peter Jones",
+        job:"intern",
+        image:"https://images2.imgbox.com/56/88/oJvFN3l5_o.jpeg",
+        text:"Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa."
+    },
+
+    {
+        id:4,
+        name:"Bill Andersn",
+        job: "The Boss",
+        image:"https://images2.imgbox.com/8b/1c/vwWNTsCd_o.jpeg",
+        text:"Edison bulp put a bird on it humblebrag, marfa pok pok heirloom fashion axe crazy stumptown venmo actually seitan.",
+    }
+];
+
+
+const name = document.getElementById("name");
+console.log(name);
+
+const job = document.getElementById("name");
+console.log(job);
+
+const text = document.getElementById("text");
+console.log(text);
+
+const button =document.getElementById("button");
+console.log(button);
+
+let index = 1
+
+button1.addEventListener("click")
